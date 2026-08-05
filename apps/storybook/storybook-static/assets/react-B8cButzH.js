6e1cd381f8ce563ebe8d1163e058f5e00289dda7
diana-lumina <diana.morales@luminasoftware.com>
2026-08-05 13:06:23 -0600

    fix: contaminación de Tailwind en radius/spacing, filtro de alias en Radius story

--- apps/storybook/storybook-static/assets/react-B8cButzH.js
+++
@@ -1,1 +0,0 @@
-import{n as e,t}from"./DocsRenderer-LL677BLK-D7h9QeWg.js";t();export{e as MDXProvider};
---
+++ apps/storybook/storybook-static/assets/react-CnVxBgMQ.js
@@ -0,0 +1,1 @@
+import{n as e,t}from"./DocsRenderer-LL677BLK-By8svdek.js";t();export{e as MDXProvider};
